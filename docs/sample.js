b65516ae31e9bf2affdc9f40ad8e22038533b740
turbographics2000 <turbographics2000@yahoo.co.jp>
2016-12-23 22:39:37 +0900

    commit

---
+++ docs/sample.js
@@ -0,0 +1,29 @@
+var map;
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
+//function init()
+//{
+var opt = {
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+var mm = document.getElementById("mymap");
+map = new google.maps.Map(mm, opt);
+
+if (navigator.geolocation == null) {
+    window.alert("位置情報が利用できません。");
+    return;
+}
+navigator.geolocation.getCurrentPosition(success, error);
+//}
+function success(position) {
+    var p = new google.maps.LatLng(positon.coords.latitude,
+        positon.coords.longitude);
+    map.setCenter(p);
+}
+
+function error(e) {
+    window.alert("位置情報が利用できません。");
+    var p = new google.maps.LatLng(0, 0);
+    map.setCenter(p);
+}
