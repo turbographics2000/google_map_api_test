c50fcd1f7dd2a5b8b60bacee708e6402fb30a2f5
turbographics2000 <turbographics2000@yahoo.co.jp>
2016-12-23 22:43:09 +0900

    commit

--- docs/sample.js
+++ docs/sample.js
@@ -1,21 +1,22 @@
 var map;
 var windowWidth = window.innerWidth;
 var windowHeight = window.innerHeight;
-//function init()
-//{
-var opt = {
-    zoom: 8,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-};
-var mm = document.getElementById("mymap");
-map = new google.maps.Map(mm, opt);
 
-if (navigator.geolocation == null) {
-    window.alert("位置情報が利用できません。");
-    return;
+function init() {
+    var opt = {
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var mm = document.getElementById("mymap");
+    map = new google.maps.Map(mm, opt);
+
+    if (navigator.geolocation == null) {
+        window.alert("位置情報が利用できません。");
+        return;
+    }
+    navigator.geolocation.getCurrentPosition(success, error);
 }
-navigator.geolocation.getCurrentPosition(success, error);
-//}
+
 function success(position) {
     var p = new google.maps.LatLng(positon.coords.latitude,
         positon.coords.longitude);
@@ -27,3 +28,5 @@ function error(e) {
     var p = new google.maps.LatLng(0, 0);
     map.setCenter(p);
 }
+
+init();
